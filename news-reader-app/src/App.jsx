0e5fee99237f822b795dac902d2deb96a987c3f5
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-23 15:05:01 -0700

    Add components with new features and styling

--- news-reader-app/src/App.jsx
+++ news-reader-app/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import useFetch from "./hooks/useFetch";
+import NewsCard from "./components/NewsCard";
+// import LoadingState from "./components/LoadingState"
 
-function App() {
-  const [count, setCount] = useState(0)
+//prettier-ignore
+const App = () => {
+  const { data, error, isLoading } = useFetch( "https://newsapi.org/v2/everything?q=apple&from=2025-03-19&to=2025-03-19&sortBy=popularity&pageSize=10&apiKey=6eea67bca3884f1ab90372ac184ac945" );
+
+  if (isLoading){ 
+    const arr = [1,1,1,1,1,1,]
+    return (
+      <main className="min-h-screen flex items-center justify-center bg-gray-50 py-20">
+        <section className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[5%]">
+          {/* {arr.map(()=> <LoadingState key={Math.random()}/>)} */}
+        </section>
+      </main>
+    )
+  };
+
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 py-20">
+      <section className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[5%]">
+        {data.map((article) => (
+          <NewsCard key={article.id} id={article.id} title={article.title} author={article.author} urlToImage={article.urlToImage} description={article.description} publishedAt={article.publishedAt} />
+        ))}
+      </section>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- news-reader-app/src/main.jsx
+++ news-reader-app/src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ArticlesDetails from "./components/ArticlesDetails.jsx";
+import ArticlesDetails from "./components/ArticleDetails.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
